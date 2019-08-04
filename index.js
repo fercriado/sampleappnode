04dda708bc9a2a01772c3f6fff2123901026788e
Fernando Criado <53142664+fercriado@users.noreply.github.com>
2019-08-04 02:55:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+var server = http.createServer((req, res) => {
+    var remIP = res.socket.remoteAddress;
+    var remPort = res.socket.remotePort;
+    var msg = 'Your IP address is ' + remIP + ' and your source port is ' + remPort + '.';
+    console.log(msg);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(msg);
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d\n", port);
+console.log('Server running at http://localhost:%d\n', port);
